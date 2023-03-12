8e95f386029e0baba09652bba42b6393bbdf1026
jlidrych <jlidrych@gmail.com>
2023-03-12 17:34:05 +0100

    fix issue in definitions.py

--- python/definitions.py
+++ python/definitions.py
@@ -53,13 +53,13 @@ JECTagDatabase = {
         "D_preVFP":"Summer19UL16APV_RunBCD_V7_DATA",
         "E_preVFP":"Summer19UL16APV_RunEF_V7_DATA",
         "F_preVFP":"Summer19UL16APV_RunEF_V7_DATA"
-     }
+     },
     "2016ULpostVFP":{
         "MC":"Summer19UL16_V7_MC",
         "F_postVFP":"Summer19UL16_RunFGH_V7_DATA",
         "G_postVFP":"Summer19UL16_RunFGH_V7_DATA",
         "H_postVFP":"Summer19UL16_RunFGH_V7_DATA"
-     }
+     },
     "2017UL":{
         "MC":"Summer19UL17_V5_MC",
         "B":"Summer19UL17_RunB_V5_DATA",
@@ -67,14 +67,14 @@ JECTagDatabase = {
         "D":"Summer19UL17_RunD_V5_DATA",
         "E":"Summer19UL17_RunE_V5_DATA",
         "F":"Summer19UL17_RunF_V5_DATA"
-     }
+     },
     "2018UL":{
         "MC":"Summer19UL18_V5_MC",
         "A":"Summer19UL18_RunA_V5_DATA",
         "B":"Summer19UL18_RunB_V5_DATA",
         "C":"Summer19UL18_RunC_V5_DATA",
         "D":"Summer19UL18_RunD_V5_DATA"
-     }
+     },
 }
 JERTagDatabase = {
     "2016ULpreVFP": "Summer20UL16APV_JRV3_MC",
